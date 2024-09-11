eb9696f5fde9df91e84c5e80dd4a8f8355a80d0b
AnaBelenBarbero <125821501+AnaBelenBarbero@users.noreply.github.com>
2024-09-11 05:55:49 +0200

    add uuid generator

---
+++ easy_bookmarks/stores/utils.py
@@ -0,0 +1,15 @@
+import uuid
+import hashlib
+
+
+def generate_uuid(input_str: str) -> uuid.UUID:
+    # Convert the string to bytes if it's not already
+    input_str = input_str if isinstance(input_str, bytes) else input_str.encode("utf-8")
+
+    # Create a MD5 hash of the input string
+    hash_object = hashlib.md5(input_str)
+    hash_hex = hash_object.hexdigest()
+
+    generated_uuid = uuid.UUID(hash_hex)
+
+    return generated_uuid
