a8dd52676edde5b053f0cd6981efb819a894f16d
CarlosVecina <carlosvecinatebar@gmail.com>
2024-09-11 12:45:55 +0200

    ✨ add base digestor

---
+++ easy_bookmarks/digestors/base_digestor.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from pydantic import BaseModel
+
+
+class BaseDigestor(BaseModel, ABC):
+    model_client: Any | None
+    model_kwargs: dict | None = {"model": "gpt-4o"}
+
+    _template: str = r"""
+    <SYS>{{task_desc_str}}</SYS>
+    User: {{input_str}}
+    You:"""
+
+    @abstractmethod
+    def summarize(self, text: str) -> str: ...
